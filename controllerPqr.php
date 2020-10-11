e4b0793436d5080615b59a14e443c111db1dcf34
juangarzon616 <jpgarzon947@misena.edu.co>
2020-10-10 20:39:58 -0500

    frth

---
+++ controllerPqr.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting(0);
+header('Content-Type: application/json');
+require('../modelo/ConsultasPqr.php');
+
+class ControllerPqr{
+    public $asunto;
+    public $descripcion;
+    public $tipPqr;
+    public $idEmpresa;
+    public $numUser;
+    public $tipNumUser;
+    public $nitEmpresa;
+
+    public function __construct(){
+        if(!empty($_POST['asunto'])){
+            $this->asunto      = $_POST['asunto'];
+            $this->descripcion = $_POST['descripcion'];
+            $this->tipPqr      = $_POST['tipqr'];
+            $this->idEmpresa   = $_POST['idempresa'];
+            $this->numUser     = $_POST['numuser'];
+            $this->tipNumUser  = $_POST['tipdoc'];
+            $this->nitEmpresa  = $_POST['nitempresa'];
+        }
+    }
+
+
+    public function createPqrController(){
+        $respuesta1 = ConsultasPqr::creatPqrModel($this, "pqr");
+        return $respuesta1;
+    }
+
+
+    public function readPqrController(){
+        $datos = array("num" => $_GET['numuser'], "tipdoc" => $_GET['tipdoc']);
+        $respuesta = ConsultasPqr::readPqrModel($datos,"pqr");
+        return $respuesta;
+    }
+
+    public function updatePqrController(){
+        $datosController = array("id_pqr" => $_GET['idpqr'], "estado" => $_GET['estado'], "respuesta" => $_GET['respuestacion']);
+        $respuesta = ConsultasPqr::updatePqrModel($datosController, "pqr");
+        return $respuesta;
+    }
+
+    public function deletePqrController(){
+        $datosController = array("idpqr" => $_GET['idpqr']);
+        $respuesta = ConsultasPqr::deletePqrModel($datosController, "pqr");
+        return $respuesta;
+    }
+
+}
+
+?>
---
+++ controllerempresa.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'consultaEmpresa.php';
+/**
+ * 
+ */
+class ControllerEmpresa
+{
+    public function createEmpresaController(){
+        $respuesta1 = ConsultasEmpresa::createEmpresaModel($this, "empresas");
+        return $respuesta1;
+    }
+}
+
+public function readEmpresaController(){
+    $respuesta = Datos::readEmpresaModel("empresas");
+    return $respuesta;
+}
+
+public function updateEmpresaController($id_empresa, $contraseña_empresa){
+    $datosController = array("id_empresa"=>$id_empresa, "contraseña_empresa"=>$contraseña_empresa);
+    $respuesta = Datos::updateEmpresaModel($datosController, "empresas");
+    return $respuesta;
+}
+
+public function deleteEmpresaController($id_empresa){
+    $respuesta = Datos::deleteEmpresaModel($id_empresa, "empresas");
+    return $respuesta;
+}
+
+public function loginEmpresaController($nom_empresa, $contraseña_empresa){
+    $datosController = array("nom_empresa" => $nom_empresa, "contraseña_empresa" => $contraseña_empresa);
+
+    $respuesta = Datos::loginEmpresaModel($datosController, "empresas");
+    return $respuesta;
+}
+
+?>
